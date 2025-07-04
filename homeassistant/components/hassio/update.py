bda9bce94656db643f7a2f45df81d96c0b3de251
wissamoh <wissamhamdash@hotmail.com>
2025-07-04 07:31:48 +0300

    Update titles for Home Assistant OS, Supervisor, and Core in update.py

--- homeassistant/components/hassio/update.py
+++ homeassistant/components/hassio/update.py
@@ -163,7 +163,7 @@ class SupervisorOSUpdateEntity(HassioOSEntity, UpdateEntity):
         | UpdateEntityFeature.SPECIFIC_VERSION
         | UpdateEntityFeature.BACKUP
     )
-    _attr_title = "Smartelligent Operating System"
+    _attr_title = "Home Assistant Operating System"
 
     @property
     def latest_version(self) -> str:
@@ -201,7 +201,7 @@ class SupervisorSupervisorUpdateEntity(HassioSupervisorEntity, UpdateEntity):
     """Update entity to handle updates for the Home Assistant Supervisor."""
 
     _attr_supported_features = UpdateEntityFeature.INSTALL
-    _attr_title = "Smartelligent Supervisor"
+    _attr_title = "Home Assistant Supervisor"
 
     @property
     def latest_version(self) -> str:
@@ -251,7 +251,7 @@ class SupervisorCoreUpdateEntity(HassioCoreEntity, UpdateEntity):
         | UpdateEntityFeature.SPECIFIC_VERSION
         | UpdateEntityFeature.BACKUP
     )
-    _attr_title = "Smartelligent Core"
+    _attr_title = "Home Assistant Core"
 
     @property
     def latest_version(self) -> str:
